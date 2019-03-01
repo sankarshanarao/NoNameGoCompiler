db536bba572c422ee50a7c8179e9cf272a1a3467
Srikar <saisrikark@gmail.com>
2019-03-01 15:55:45 +0530

    Nested if else is also working

--- parser.hpp
+++ parser.hpp
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,127 +30,93 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_YY_PARSER_HPP_INCLUDED
+# define YY_YY_PARSER_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     TIDENTIFIER = 258,
-     TINTEGER = 259,
-     TDOUBLE = 260,
-     TCEQ = 261,
-     TCNE = 262,
-     TCLT = 263,
-     TCLE = 264,
-     TCGT = 265,
-     TCGE = 266,
-     TEQUAL = 267,
-     TNEG = 268,
-     TBAND = 269,
-     TBOR = 270,
-     TBXOR = 271,
-     TAND = 272,
-     TOR = 273,
-     TLCHAN = 274,
-     TRCHAN = 275,
-     TLPAREN = 276,
-     TRPAREN = 277,
-     TLBRACE = 278,
-     TRBRACE = 279,
-     TCOMMA = 280,
-     TDOT = 281,
-     TPLUS = 282,
-     TMINUS = 283,
-     TMUL = 284,
-     TDIV = 285,
-     TKFOR = 286,
-     TKIF = 287,
-     TKFUNC = 288,
-     TKIMPORT = 289,
-     TKMAIN = 290,
-     TKPACKAGE = 291,
-     TKVAR = 292,
-     TDINT = 293,
-     TDFLOAT = 294,
-     TDBOOL = 295,
-     TDSTRING = 296,
-     BoolTrue = 297,
-     BoolFalse = 298,
-     String = 299,
-     Float = 300,
-     Int = 301,
-     MultiCommentBegin = 302,
-     MultiCommentEnd = 303,
-     Identifier = 304
-   };
+  enum yytokentype
+  {
+    TIDENTIFIER = 258,
+    TINTEGER = 259,
+    TDOUBLE = 260,
+    TCEQ = 261,
+    TCNE = 262,
+    TCLT = 263,
+    TCLE = 264,
+    TCGT = 265,
+    TCGE = 266,
+    TEQUAL = 267,
+    TNEG = 268,
+    TBAND = 269,
+    TBOR = 270,
+    TBXOR = 271,
+    TAND = 272,
+    TOR = 273,
+    TLCHAN = 274,
+    TRCHAN = 275,
+    TLPAREN = 276,
+    TRPAREN = 277,
+    TLBRACE = 278,
+    TRBRACE = 279,
+    TCOMMA = 280,
+    TDOT = 281,
+    TPLUS = 282,
+    TMINUS = 283,
+    TMUL = 284,
+    TDIV = 285,
+    TKFOR = 286,
+    TKIF = 287,
+    TKFUNC = 288,
+    TKIMPORT = 289,
+    TKMAIN = 290,
+    TKPACKAGE = 291,
+    TKVAR = 292,
+    TKELSE = 293,
+    TDINT = 294,
+    TDFLOAT = 295,
+    TDBOOL = 296,
+    TDSTRING = 297,
+    BoolTrue = 298,
+    BoolFalse = 299,
+    String = 300,
+    Float = 301,
+    Int = 302,
+    MultiCommentBegin = 303,
+    MultiCommentEnd = 304,
+    Identifier = 305
+  };
 #endif
-/* Tokens.  */
-#define TIDENTIFIER 258
-#define TINTEGER 259
-#define TDOUBLE 260
-#define TCEQ 261
-#define TCNE 262
-#define TCLT 263
-#define TCLE 264
-#define TCGT 265
-#define TCGE 266
-#define TEQUAL 267
-#define TNEG 268
-#define TBAND 269
-#define TBOR 270
-#define TBXOR 271
-#define TAND 272
-#define TOR 273
-#define TLCHAN 274
-#define TRCHAN 275
-#define TLPAREN 276
-#define TRPAREN 277
-#define TLBRACE 278
-#define TRBRACE 279
-#define TCOMMA 280
-#define TDOT 281
-#define TPLUS 282
-#define TMINUS 283
-#define TMUL 284
-#define TDIV 285
-#define TKFOR 286
-#define TKIF 287
-#define TKFUNC 288
-#define TKIMPORT 289
-#define TKMAIN 290
-#define TKPACKAGE 291
-#define TKVAR 292
-#define TDINT 293
-#define TDFLOAT 294
-#define TDBOOL 295
-#define TDSTRING 296
-#define BoolTrue 297
-#define BoolFalse 298
-#define String 299
-#define Float 300
-#define Int 301
-#define MultiCommentBegin 302
-#define MultiCommentEnd 303
-#define Identifier 304
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 19 "parser.y"
+
+union YYSTYPE
 {
+#line 15 "parser.y" /* yacc.c:1909  */
+
     std::string *string;
     int token;
-}
-/* Line 1529 of yacc.c.  */
-#line 152 "parser.hpp"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 110 "parser.hpp" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_PARSER_HPP_INCLUDED  */
--- parser.y
+++ parser.y
@@ -1,9 +1,5 @@
 %{
     #include <iostream>
-    /*
-    #include "node.h"
-    NBlock *programBlock; *//* the top level root node of our final AST */
-
     extern int yylex();
     void yyerror(const char *s) { printf("ERROR: %sn", s); }
 %}
@@ -12,7 +8,7 @@
 %token <token> TCEQ TCNE TCLT TCLE TCGT TCGE TEQUAL TNEG TBAND TBOR TBXOR TAND TOR TLCHAN TRCHAN
 %token <token> TLPAREN TRPAREN TLBRACE TRBRACE TCOMMA TDOT
 %token <token> TPLUS TMINUS TMUL TDIV
-%token <token> TKFOR TKIF TKFUNC TKIMPORT TKMAIN TKPACKAGE TKVAR
+%token <token> TKFOR TKIF TKFUNC TKIMPORT TKMAIN TKPACKAGE TKVAR TKELSE
 %token <token> TDINT TDFLOAT TDBOOL TDSTRING
 %token <token> BoolTrue BoolFalse String Float Int MultiCommentBegin MultiCommentEnd Identifier
 
@@ -27,11 +23,126 @@
 %start program
 
 %%
-program:
-    PackageDefinition '\n'
-    ;
 
+
+
+//Starting part of parsing...........
+program:
+    PackageDefinition
+    Imports
+    Declaration
+    MainFunc
+    Declaration
+    ;
+//....................................
+
+
+
+//Initial package identifier..........
 PackageDefinition: TKPACKAGE TKMAIN
     | TKPACKAGE TIDENTIFIER
+    | PackageDefinition '\n'
     ;
+//.....................................
+
+
+
+//Imports part.........................
+Imports:
+    SingleImports
+    ;
+SingleImports:
+    SingleImportsDash
+    ;
+SingleImportsDash:
+    TKIMPORT String '\n' SingleImportsDash
+    |
+    ;
+//......................................
+
+
+
+//Entire part for declarations
+Declaration:
+    Declarationdash
+    ;
+
+Declarationdash:
+    VariableDecla  Declarationdash
+    |
+    ; 
+
+VariableDecla:
+    Identifier TEQUAL Literal '\n'
+    ;
+
+ConditionalExp:
+    ;
+//............................................
+
+
+
+//Miscellaneous..............................
+Literal:
+    BoolTrue
+    | BoolFalse
+    | String
+    | Float
+    | Int
+    ;
+//...........................................
+
+
+
+//Main and it's immediate inside...............
+MainFunc:
+    TKFUNC TKMAIN TLPAREN TRPAREN CompoundBloc
+    ;
+
+CompoundBloc:
+    TLBRACE  '\n'  
+        StatementList
+    TRBRACE CompoundBloc
+    | '\n'
+    |
+    ;
+
+StatementList:
+    StatementList Statement
+    |
+    ;
+
+Statement:
+    Declaration
+    | For
+    | If
+    ;
+//...............................................
+
+
+
+//for implementation.............................
+For:
+    TKFOR TLPAREN  TRPAREN CompoundBloc
+    ;
+//...............................................
+
+
+
+//if else implementation.........................
+If:
+    TKIF TLPAREN TRPAREN CompoundBloc
+    Else
+    ;
+
+//To eliminate left recursion
+Else:
+    TKELSE TKIF TLPAREN TRPAREN CompoundBloc
+    Else
+    | TKELSE CompoundBloc
+    |
+    ;
+//...............................................
+
+
 %%
